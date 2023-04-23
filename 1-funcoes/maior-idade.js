4ec5b4ab1b2e3afb91e25cc186f12d23894eaf27
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-04-23 15:52:15 -0300

    att branch

--- 1-funcoes/maior-idade.js
+++
@@ -1,9 +0,0 @@
-function verificarIdade(idade, nome) {
-  if(idade >= 18) {
-    console.log(`Meu nome é ${nome} e sou maior de idade.`);
-  } else {
-    console.log(`Meu nome é ${nome} e sou maior de idade.`);
-  }
-}
-
-verificarIdade(28, "Henrique");
